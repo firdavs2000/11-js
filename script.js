442b3f804804149efcf40258424a752a30dcb76a
Azizpro <azizbek.murodov6361@gmail.com>
2025-01-29 16:33:33 +0500

    index.ktml  obj.js  script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+function calculateOrder() {
+    let order1 = cartObj(); 
+    // let order2 = cartObj(); 
+    
+    let totalPrice1 = 0;
+    // let totalPrice2 = 0;
+  
+    let deliveryPrice1 = 9000;  
+    // let deliveryPrice2 = 9000;    
+    console.log('Sizning buyurtmangiz:');
+  
+    
+    console.log("1-buyurtma:");
+    for (let item in order1) {
+      let product = order1[item];
+      console.log(`${item} (${product.info}): ${product.price} so'm`);
+      totalPrice1 += product.price;
+    }
+    
+  
+    // console.log("2-buyurtma:");
+    // for (let item in order2) {
+    //   let product = order2[item];
+    //   console.log(`${item} (${product.info}): ${product.price} so'm`);
+    //   totalPrice2 += product.price;
+    // }
+  
+  
+    console.log(`1-buyurtma yetkazib berish narxi: ${deliveryPrice1} so'm`);
+    // console.log(`2-buyurtma yetkazib berish narxi: ${deliveryPrice2} so'm`);
+  
+  
+    let totalOrderPrice1 = totalPrice1 + deliveryPrice1;
+    // let totalOrderPrice2 = totalPrice2 + deliveryPrice2;
+  
+    console.log(`1 buyurtmaning jami narxi (yetkazib berish bilan): ${totalOrderPrice1} so'm`);
+    // console.log(`2 buyurtmaning jami narxi (yetkazib berish bilan): ${totalOrderPrice2} so'm`);
+  
+  
+    let orderSum = totalOrderPrice1 ;
+    console.log(`Jami buyurtmalar narxi: ${orderSum} so'm: yetkazib berish narxi:9000 som`);
+
+  }
+  
+  calculateOrder();
